e6ba672da472526bd29efc6af661f1aaab3d52f8
Program <cntprprb253@gmail.com>
2025-12-17 00:12:28 +0000

    first commit

---
+++ chatbot_sesion1.cpp
@@ -0,0 +1,23 @@
+/*Inicializa correctamente*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nombre;
+    string empresa = "OfiExpres";
+
+    cout << "=== Chatbot " << empresa << " v2 ===" << endl;
+    cout << "Hola, soy el asistente virtual de " << empresa << "." << endl;
+    cout << "Para comenzar, dime tu nombre: ";
+
+    getline(cin, nombre);
+
+    //nombre = "cliente"; variable Invalida 
+    //Cliente se puede nombrar en la linea de salida.
+    cout << "Mucho gusto, "  << "Cliente" << nombre << ". Gracias por contactar a "
+         << empresa << "." << endl;
+    cout << "En esta version ya puedes consultar pedidos e informacion real." << endl;
+
+    return 0;
+}
