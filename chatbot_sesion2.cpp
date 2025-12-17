eaa96ff28a0d516485829699edaa407d2574a505
Program <cntprprb253@gmail.com>
2025-12-17 01:12:24 +0000

    proyrcyo corregido

---
+++ chatbot_sesion2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nombre;
+    string opcion;
+    string empresa = "OfiExpress";
+
+    cout << "=== Chatbot " << empresa << " v2 ===" << endl;
+    cout << "Hola, soy el asistente virtual de " << empresa << "." << endl;
+    cout << "Como te llamas? ";
+    getline(cin, nombre);
+
+    cout << "Hola, " << nombre << ". En que puedo ayudarte hoy?" << endl;
+    cout << "Escribe: 'horario', 'servicio' o 'contacto': ";
+    getline(cin, opcion); 
+
+    if (opcion == "horario") {
+        cout << "Puedes comunicarte al telefono 55-1234-5678 o al correo ventas@ofiexpress.mx" << endl;
+    } else if (opcion == "servicios") {
+        cout << "Ofrecemos venta y reparto de articulos de oficina para PYMES." << endl;
+    } else if (opcion == "contacto") {
+        cout << "Nuestro horario de atencion es de lunes a viernes de 9:00 a 18:00." << endl;
+    } else {
+        cout << "Lo siento, aun no tengo informacion sobre ese tema." << endl;
+    }
+
+    return 0;
+}
---
+++ chatbot_sesion3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string empresa = "OfiExpress";
+    string nombre;
+    string mensaje;
+
+    cout << "=== Chatbot " << empresa << " v3 ===" << endl;
+    cout << "Hola, soy el asistente virtual de " << empresa << "." << endl;
+    cout << "Como te llamas? ";
+    getline(cin, nombre);
+
+    cout << "Escribe 'horario', 'contacto', 'servicios' o 'salir' para terminar." << endl;
+
+    while (true) {
+        cout << "Tu: ";
+        getline(cin, mensaje);
+
+        if (mensaje == "horario") {
+            cout << "Bot: Tel: 55-1234-5678 | Correo: ventas@ofiexpress.mx" << endl;
+        } else if (mensaje == "contacto") {
+            cout << "Bot: Atendemos de lunes a viernes de 9:00 a 18:00." << endl;
+        } else if (mensaje == "servicios") {
+            cout << "Bot: Gestion de pedidos y reparto rapido de articulos de oficina." << endl;   
+        } else if (mensaje == "salir ") {
+            cout << "Bot: Gracias por visitar " << empresa << ". Hasta luego." << endl;
+            break;        
+        } else{
+            cout << "Bot: Aun no estoy programado para esa pregunta." << endl; //error se presenta e el mensaje de salida.
+            }   
+    }
+    return 0;
+}
